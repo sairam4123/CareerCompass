e26bf6c01eee53bf333846f64e17b9a8850949da
Sairam M <sairam.m.2005.65@gmail.com>
2025-01-19 18:39:00 +0530

    Add react-toastify for notifications; enhance Result page with share functionality and loading indicators

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import cn from "../utils/cn";
 import Spinner from "./Spinner";
+import React from "react";
 
 export default function Button({
   className,
@@ -7,7 +8,8 @@ export default function Button({
   children,
   isLoading,
   text,
-  disabled = false
+  disabled = false,
+  icon
 }: {
   className?: string;
   onClick: () => void;
@@ -15,6 +17,7 @@ export default function Button({
   isLoading?: boolean;
   disabled?: boolean;
   text: string;
+  icon?: React.ReactNode;
 }) {
     const defaultClassName = "flex items-center gap-4 justify-center bg-dark flex-row text-white p-2 rounded-lg mt-4 transition-all min-w-fit w-full md:w-2/4 lg:w-2/5 xl:w-1/6 hover:md:w-3/4 hover:lg:w-3/5 hover:xl:w-1/4 hover:bg-light-dark";
   return (
@@ -22,7 +25,7 @@ export default function Button({
       onClick={onClick}
       className={cn(defaultClassName, className, disabled && "cursor-not-allowed opacity-50")}
     >
-      {children ? children : <>{isLoading && <Spinner size="small" color="black" />}{text}</>}
+      {children ? children : <>{isLoading && <Spinner size="small" color="black" />}{icon}{text}</>}
     </button>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Result from './pages/Result.tsx'
+import { NavBar } from './components/NavBar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/result/:userId" element={<Result />} />
+      <Route path="/result/:userId" element={<><NavBar /><Result /></>} />
     </Routes>
     </BrowserRouter>
   </StrictMode>,
